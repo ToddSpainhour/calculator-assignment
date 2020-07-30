ecbf3fb8d3726736cbd313988d1f26ebaae529d7
Todd Spainhour <toddspainhour@gmail.com>
2020-07-29 21:05:14 -0500

    formatted code and text within

--- calculator-assignment/Program.cs
+++ calculator-assignment/Program.cs
@@ -9,6 +9,10 @@ namespace calculator_assignment
         {
             Console.WriteLine(" ");
             string linebreak = "----------------------";
+
+
+
+
             // intro 
 
             Console.WriteLine(" ");
@@ -16,6 +20,8 @@ namespace calculator_assignment
             Console.WriteLine(" ");
             Console.WriteLine("This first section takes a array of comma-seperated numbers (that start as strings) and turns them into integers");
             Console.WriteLine(" ");
+
+
 
 
             // create a comma seperated list of strings
@@ -27,14 +33,18 @@ namespace calculator_assignment
 
 
 
+
             // a new list to 'push' your newly converted ints into
 
             List<int> commaSeperatedListOfIntegers = new List<int>();
 
 
 
+
             // turn comma seperated list of strings into numbers
+
             Console.WriteLine("Inside the foreach loop of your original, comma seperated array called commaSeperatedListOfStrings returns...");
+
             foreach (var digit in commaSeperatedListOfStrings)
             {
                 commaSeperatedListOfIntegers.Add(int.Parse(digit));
@@ -48,6 +58,9 @@ namespace calculator_assignment
 
 
 
+
+            // convert strings into integers
+
             Console.WriteLine("Now let's convert those strings to ints!");
             Console.WriteLine("");
 
@@ -58,6 +71,10 @@ namespace calculator_assignment
             }
 
 
+
+
+            // multiply those ints together
+
             Console.WriteLine(linebreak);
             Console.WriteLine(" ");
             Console.WriteLine("Now we need to multiply them in order. For example, if we had '2, 3, 2' it should return 12 (2x3x2)");
@@ -67,12 +84,13 @@ namespace calculator_assignment
 
 
             int result = 1;
+            Console.WriteLine($"Your 'result' variable above the foreach loops is... {result}");
 
             foreach (var individualNumber in commaSeperatedListOfIntegers)
             {
                 Console.WriteLine($"This number is... {individualNumber}");
                 result *= individualNumber;
-                Console.WriteLine($"Inside our foreach loop to multiply the numbers together, we get: {result}");
+                Console.WriteLine($"Inside our foreach loop to multiply each individual number by the result, now we get: {result}");
             }
 
             Console.WriteLine("");
@@ -82,6 +100,8 @@ namespace calculator_assignment
             Console.WriteLine(linebreak);
 
             Console.ReadKey();
+
+
 
 
 
